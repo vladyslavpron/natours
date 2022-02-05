df39a0e9246230f35c69dd8843be69196d2e4cee
vladyslavpron <vladyslavpron@gmail.com>
2022-02-05 18:34:01 +0200

    first commit

---
+++ public/js/stripe.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51KPaGEDlJevs8mse9LEBtbrjGEYBgltYyz5JMAv5WCjJA7G8P3uEKyOOnwZMfM5IUr3TVFX6X3mQHpvhljbVkaNM00XAOdZvgA'
+);
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
